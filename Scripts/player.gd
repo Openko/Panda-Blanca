0d433c9fd6f75945ffc23bfa3c81b05a09431f70
Openko <56291571+Openko@users.noreply.github.com>
2025-05-07 16:19:55 +0200

    Animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,22 +10,27 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2D.play("Jump")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	
 	if direction > 0:
 		animated_sprite_2D.flip_h = false
 	elif direction < 0:
 		animated_sprite_2D.flip_h = true
 		
-	if direction == 0:
-		animated_sprite_2D.play
+	if direction == 0 && is_on_floor():
+		animated_sprite_2D.play("Idle")
+	elif direction != 0 && is_on_floor():
+		animated_sprite_2D.play("Move")
+	
 	
 	if direction:
 		velocity.x = direction * SPEED
