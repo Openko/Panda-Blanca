7a89888364996542387f065f9659e14fb47cb13a
Openko <56291571+Openko@users.noreply.github.com>
2025-05-08 18:34:56 +0200

    FInal

--- Panda_NPC.gd
+++ Panda_NPC.gd
@@ -13,27 +13,41 @@ var is_player_in_range = false
 @export var dialog_ENG_3 : String
 
 func _on_chat_detection_area_body_entered(body):
+	
 	if body.get_name() == "Player":
+		$Label.visible = true
 		is_player_in_range = true
 	pass # Replace with function body.
 
 
 func _on_chat_detection_area_body_exited(body):
+	
 	if body.get_name() == "Player":
+		$Label.visible = false
 		is_player_in_range = false
 		print("exited")
 		$DialogBox.visible = false
 	pass # Replace with function body.
 
 func _ready():
-	print()
+	$Label.visible = false
 	if(Langue.id_lang == 0):
 		$DialogBox/Text.text = dialog_FR_1
 	elif(Langue.id_lang == 1):
+		$Label.text = "Premeu E per parlar"
 		$DialogBox/Text.text = dialog_CAT_1
 	$DialogBox.visible = false
 
 func _process(delta):
+	if(Langue.id_lang == 0 && Langue.game_step == 1):
+		$DialogBox/Text.text = dialog_FR_1
+	elif(Langue.id_lang == 1 && Langue.game_step == 1):
+		$DialogBox/Text.text = dialog_CAT_1
+	
+	if(Langue.id_lang == 0 && Langue.game_step == 2):
+		$DialogBox/Text.text = dialog_FR_2
+	elif(Langue.id_lang == 1 && Langue.game_step == 2):
+		$DialogBox/Text.text = dialog_CAT_2
 	if Input.is_action_just_pressed("interact") && is_player_in_range :
 		
 		if $DialogBox.visible == false:
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,5 @@
 extends CharacterBody2D
 
-signal MusicJungle
-signal MusicVillage 
-
-
-
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -850.0
@@ -50,14 +45,3 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-
-func _on_area_village_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	MusicVillage.emit()
-
-	pass # Replace with function body.
-
-
-func _on_area_jungle_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	MusicJungle.emit()
-	pass # Replace with function body.
